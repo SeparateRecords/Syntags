2ed7b8387bb038e2b1bd0157bd9bd09d71d79a7d
SeparateRecords <52195359+SeparateRecords@users.noreply.github.com>
2020-03-11 16:19:10 +1100

    🔥 Remove `x` alias from xml namespace

--- syntags/xml.py
+++ syntags/xml.py
@@ -1,33 +1,8 @@
-"""Implements the <?xml?> prolog, exports function ``x`` to create XML elements.
+"""Implements the <?xml?> prolog."""
 
-Elements can be defined inline or bound to local names. ``x()`` uses
-``functools.lru_cache()``, meaning ``x("example")`` will always return the same object.
-Binding to local names may be helpful when defining a custom namespace.
+__all__ = ["xml"]
 
-Examples of the two methods are below.
-
-    >>> tree = x("root") [
-    ...     x("element") [
-    ...         x("myThing"),
-    ...         x("leafNode")
-    ...     ]
-    ... ]
-
-    >>> root = x("root")
-    >>> element = x("element")
-    >>> myThing = x("myThing")
-    >>> leafNode = x("leafNode")
-    >>> tree = root [
-    ...     element [
-    ...         myThing,
-    ...         leafNode
-    ...     ]
-    ... ]
-"""
-
-__all__ = ["xml", "x"]
-
-from syntags.lib.elements import XMLElement, xml_element as x
+from syntags.lib.elements import XMLElement
 
 
 class xml(XMLElement):
